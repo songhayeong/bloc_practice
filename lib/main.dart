8756d3cb16bd2ca6658fefc277a16c0985015ce2
songhayeong <sosscvs@gmail.com>
2024-02-08 14:06:59 +0900

    login bloc practice

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:bloc_practice/app.dart';
+
+void main() {
+  runApp(const App());
+}
---
+++ lib/splash/splash.dart
@@ -0,0 +1,1 @@
+export 'package:bloc_practice/splash/view/splash_page.dart';
